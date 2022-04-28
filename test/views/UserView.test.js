ed94e8c7f993bafd1f87144eedcde55b34bc6864
habacuc <josehabacuc182020@gmail.com>
2022-04-28 15:19:40 -0500

    add test 2 for UserView

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -8,7 +8,16 @@ describe( 'Suite test for class UserView', () => {
         const result = UserView.createUser( payload )
 
         // ? https://jestjs.io/docs/using-matchers#strings
-        expect( result.error ).toMatch(/payload no existe/)
+        expect( result.error ).toMatch( /payload no existe/ )
+
+    } )
+
+    test( '2) Return an error object when try to create a new user with a payload with invalid props', () => {
+
+        const payload = { username: null, name: 12, id: 'id' }
+        const result = UserView.createUser( payload )
+
+        expect( result.error ).toMatch( /necesitan tener un valor válido/ )
 
     } )
 
