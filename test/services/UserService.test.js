bafbfdd0b06f523be42b37ccb70ecfda6214c564
habacuc <josehabacuc182020@gmail.com>
2022-04-28 10:38:36 -0500

    test 4

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -35,4 +35,18 @@ describe( 'Unit test for class UserService', () => {
 
     } )
 
+    test( '4) Given a list of users give me the list of usernames', () => {
+
+        const user1 = UserService.create( 1, 'habacucsoto1', 'Habacuc Soto' )
+        const user2 = UserService.create( 1, 'habacucsoto2', 'Habacuc Soto' )
+        const user3 = UserService.create( 1, 'habacucsoto3', 'Habacuc Soto' )
+
+        const usernames = UserService.getAllUsernames( [ user1, user2, user3 ] )
+
+        expect( usernames ).toContain( 'habacucsoto1' )
+        expect( usernames ).toContain( 'habacucsoto2' )
+        expect( usernames ).toContain( 'habacucsoto3' )
+
+    } )
+
 } )
